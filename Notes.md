5ac699234fb9d10edf320f71f21ee536df0d5e67
Cameron Olson <cmsolson75@gmail.com>
2025-02-08 14:32:06 -0800

    paint working okay

--- Notes.md
+++ Notes.md
@@ -1,3 +1,9 @@
 ## NOTES
 
-Get simple paint app working: just copy a tutorial
+I need to load the models into memory early in the process
+- This means i need the architecture in a python file
+- Load it
+- Write a prediction script:
+    - Preprocess
+    - run prediction
+    - return class name to display
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from tkinter import *
-from tkinter.colorchooser import askcolor
+from PIL import Image
+import io
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 class Paint:
     # Code Adapted from https://gist.github.com/nikhilkumarsingh/85501ee2c3d8c0cfa9d1a27be5781f06
 
-    DEFAULT_PEN_SIZE = 5.0
+    DEFAULT_PEN_SIZE = 20.0
     DEFAULT_COLOR = 'black'
+    TEXT = "HI"
 
     def __init__(self):
         self.root = Tk()
@@ -13,21 +18,16 @@ class Paint:
         self.pen_button = Button(self.root, text='pen', command=self.use_pen)
         self.pen_button.grid(row=0, column=0)
 
-        self.brush_button = Button(self.root, text='brush', command=self.use_brush)
-        self.brush_button.grid(row=0, column=1)
-
-        self.color_button = Button(self.root, text='color', command=self.choose_color)
-        self.color_button.grid(row=0, column=2)
-
-
         self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=3)
+        self.eraser_button.grid(row=0, column=1)
 
+        self.predict_button = Button(self.root, text='predict', command=self.get_canvas_array)
+        self.predict_button.grid(row=0, column=2)
 
-        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=4)
+        self.my_label = Label(self.root, text="press the button")
+        self.my_label.grid(row=0, column=3)
 
-        self.c = Canvas(self.root, bg='white', width=600, height=600)
+        self.c = Canvas(self.root, bg='white', width=560, height=560)
         self.c.grid(row=1, columnspan=5)
 
         self.setup()
@@ -37,22 +37,19 @@ class Paint:
     def setup(self):
         self.old_x = None
         self.old_y = None
-        self.line_width = self.choose_size_button.get()
+        self.line_width = self.DEFAULT_PEN_SIZE
         self.color = self.DEFAULT_COLOR
         self.eraser_on = False
         self.active_button = self.pen_button
         self.c.bind('<B1-Motion>', self.paint)
         self.c.bind('<ButtonRelease-1>', self.reset)
 
+    def update_text(self, text: str):
+        self.my_label.config(text=text)
+
+
     def use_pen(self):
         self.activate_button(self.pen_button)
-    
-    def use_brush(self):
-        self.activate_button(self.brush_button)
-    
-    def choose_color(self):
-        self.eraser_on = False
-        self.color = askcolor(color=self.color)[1]
     
     def use_eraser(self):
         self.activate_button(self.eraser_button, eraser_mode=True)
@@ -63,8 +60,18 @@ class Paint:
         self.active_button = button
         self.eraser_on = eraser_mode
     
+    def get_canvas_array(self):
+        self.c.update()
+        ps = self.c.postscript(colormode='color')
+        img = Image.open(io.BytesIO(ps.encode('utf-8'))).convert("L")
+        img_array = np.asarray(img)
+        self.update_text(str(img_array.shape))
+
+        print(img_array.shape)
+
+    
     def paint(self, event):
-        self.line_width = self.choose_size_button.get()
+        # self.line_width = 
         paint_color = 'white' if self.eraser_on else self.color
         if self.old_x and self.old_y:
             self.c.create_line(self.old_x, self.old_y, event.x, event.y,
